720ec00b9e803834d93702bdc98db3697531de88
AstinaWahyuni <72489941+AstinaWahyuni@users.noreply.github.com>
2022-02-19 03:10:10 +0700

    koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,25 @@
 <?php
-$koneksi=mysqli_connect("localhost","root","","database-siperspdg");
-// Check connection
-if (mysqli_connect_errno()){
-	echo "Koneksi database gagal : " . mysqli_connect_error();
-}
+$koneksi=mysqli_connect("54.208.203.255","admin","","database-siperspdg");
+// $koneksi=mysqli_connect("","","database-siperspdg");
+// // Check connection
+// if (mysqli_connect_errno()){
+// 	echo "Koneksi database gagal : " . mysqli_connect_error();
+// }
+ /* Connect to MySQL and select the database. */
+//  $connection = mysqli_connect(database-siperspdg.crb4qur1hich.us-east-1.rds.amazonaws.com, admin, 12345678);
+
+//  if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
+
+//  $database = mysqli_select_db($connection, database-siperspdg);
+
+//  /* Ensure that the EMPLOYEES table exists. */
+//  VerifyEmployeesTable($connection, database-siperspdg);
+
+//  /* If input fields are populated, add a row to the EMPLOYEES table. */
+//  $employee_name = htmlentities($_POST['NAME']);
+//  $employee_address = htmlentities($_POST['ADDRESS']);
+
+//  if (strlen($employee_name) || strlen($employee_address)) {
+//    AddEmployee($connection, $employee_name, $employee_address);
+//  }
 ?>
